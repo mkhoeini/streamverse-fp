2a376a51187f7d943ba3e6a91e2862dc27c5b79e
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2023-04-27 17:58:05 +0200

    fix: Fix lib name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-// https://vitejs.dev/guide/build.html#library-mode
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(import.meta.dir, "src/index.ts"),
-      name: "my-lib",
-      fileName: "my-lib",
+      name: "StreamVerseFP",
+      fileName: "streamverse-fp",
     },
   },
   plugins: [dts()],
